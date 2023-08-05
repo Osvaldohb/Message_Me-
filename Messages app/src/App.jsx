043f38b3128582ce7be609caeebb439a77503849
Osvaldo-Santillan-Jimenez <osvaldo.santillan@codigogeek.com.mx>
2023-08-05 15:55:36 -0600

    Inicio Proyecto

---
+++ Messages app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap'
+import Login from './Pages/Login/Login';
+
+const App = () => {
+  return (
+    <div> <Login/></div>
+  )
+}
+
+export default App
---
+++ Messages app/src/Pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Login.css'
+
+const Login = () => {
+  return (
+    <div className='w-100 vh-100 Login__background'>
+    
+    </div>
+  )
+}
+
+export default Login
